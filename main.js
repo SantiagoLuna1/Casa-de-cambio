e508d26657326107c828d266b2c6cbad7635f9b5
SantiagoLuna1 <santiluna010@gmail.com>
2025-02-24 14:39:44 -0300

    Casa de cambio
    Se agrega la lógica, dependencias e instrucciones en un readme.md

---
+++ main.js
@@ -0,0 +1,87 @@
+function tasasEnTiempoReal(base = 'EUR', fecha = 'latest') {
+    const BASE_URL = 'https://api.exchangeratesapi.io/v1';
+    return fetch(`${BASE_URL}/${fecha}?access_key=${CONFIG.API_KEY}&base=${base}`)
+        .then((r) => r.json())
+        .then((r) => r.rates);
+}  //la funcion hace la solicitud a la API
+
+function obtenerMonedas() {
+    return tasasEnTiempoReal().then((resultado) => Object.keys(resultado).concat('EUR')); //en la lista de monedas tenemos el Euro
+}
+
+function mostrarCambios(cambios) {
+    const $cambios = document.querySelector('#cambio tbody');
+    $cambios.innerHTML = '';
+
+    Object.keys(cambios).sort().forEach((moneda) => {
+        const $fila = document.createElement('tr'); // crea una fila
+        const $moneda = document.createElement('td');  // crea una celda para moneda
+        const $cambio = document.createElement('td');  // crea una celda para cambio
+        $moneda.textContent = moneda;  
+        $cambio.textContent = cambios[moneda];
+        $fila.appendChild($moneda);
+        $fila.appendChild($cambio);
+        $cambios.appendChild($fila);
+    });
+}
+
+function mostrarListadoMonedas(monedas) {
+    const $lista = document.createElement('div');
+    $lista.className = 'list-group'; 
+
+    monedas.sort().forEach((base) => {
+        const $item = document.createElement('a');
+        $item.href = '#';
+        $item.classList.add('list-group-item', 'list-group-item-action');
+        $item.textContent = base;
+        $item.dataset.base = base; //crea data-base
+        $item.addEventListener('click', () => {
+            const $itemActivo = document.querySelector('.list-group-item.active');
+            if ($itemActivo) {
+                $itemActivo.classList.remove('active');
+            }
+            $item.classList.add('active');
+            actualizar();
+        });
+        $lista.appendChild($item);
+    });
+
+    document.querySelector('#monedas').appendChild($lista); //todo se crea dentor del <div id="monedas">
+}
+
+function obtenerMonedaSeleccionada() {
+    const $activeItem = document.querySelector('list-group-item.active');
+    if ($activeItem) {
+        return $activeItem.dataset.base;
+    }
+
+    return undefined;
+}
+
+function obtenerFechaSeleccionada() {
+    const fechaSeleccionada = document.querySelector('#fecha').value;
+    return fechaSeleccionada || undefined;  //si no se selecciona una fecha, usa 'latest'
+}
+
+function mostrarCartelActualizacion() {
+    document.querySelector('#cambio tbody').innerHTML = 'Cargando...';
+}
+
+function actualizar() {
+    mostrarCartelActualizacion();
+    tasasEnTiempoReal(obtenerMonedaSeleccionada(), obtenerFechaSeleccionada())
+        .then((cambios) => {
+            mostrarCambios(cambios);
+        });
+}
+
+
+function inicializar() {
+    obtenerMonedas().then((monedas) => {
+        mostrarListadoMonedas(monedas);
+    });
+
+    configurarInputFecha();
+}
+
+inicializar();
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Casa de Cambio
+
+Este proyecto es una **Casa de Cambio en línea** que permite consultar el tipo de cambio de distintas monedas en tiempo real y en una fecha específica, utilizando la API de Exchange Rates.
+
+## Características
+
+- Consulta en tiempo real de las tasas de cambio entre distintas monedas.
+- Visualización de la lista de monedas disponibles para realizar la conversión.
+- Posibilidad de seleccionar la fecha para obtener datos históricos del tipo de cambio.
+- Interfaz amigable que permite ver las tasas de cambio en una tabla.
+
+## Requisitos
+
+- **Clave de API** de Exchange Rates API para realizar las consultas.
+  
+### Cómo obtener tu clave de API
+
+1. Regístrate en [Exchange Rates API](https://exchangeratesapi.io/) y obtén tu clave gratuita de API.
+2. Guarda la clave en un archivo `config.js` como se explica en la sección de **Configuración** más abajo.
+
+## Instalación
+
+1. Clona el repositorio
+2. Utiliza "npm install" para instalar dependencias.
+
+## Configuración
+
+Para configurar la clave de la API, sigue estos pasos:
+
+1. Renombra el archivo `config-ejemplo.js` a `config.js`.
+2. Abre `config.js` y reemplaza `'tu_api_key_aqui'` con tu clave de API personal.
+3. Guarda los cambios y ya está listo para usar. 
